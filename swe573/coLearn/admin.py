0260d5e21ad6ec9ea338766525b48ed1e60b32cd
Rhonur <onur.demirkale@outlook.com>
2022-05-08 16:06:55 +0300

    Implemented database models for the Co-Learn web application and registered the models for the Django admin website, closes #20

--- swe573/coLearn/admin.py
+++ swe573/coLearn/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from coLearn.models import Sample
+from coLearn.models import CoLearnUser
+from coLearn.models import Topic
+from coLearn.models import LearningSpace
+from coLearn.models import Chat
+from coLearn.models import ChatMessage
+from coLearn.models import Post
+from coLearn.models import Reply
+from coLearn.models import Quiz
+from coLearn.models import Question
+
+
+admin.site.register(Sample)
+admin.site.register(CoLearnUser)
+admin.site.register(Topic)
+admin.site.register(LearningSpace)
+admin.site.register(Chat)
+admin.site.register(ChatMessage)
+admin.site.register(Post)
+admin.site.register(Reply)
+admin.site.register(Quiz)
+admin.site.register(Question)
+
+
+
+
+
+
