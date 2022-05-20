c6df5803eeb4975e65f4dc72a530d2b8183afd74
Rhonur <onur.demirkale@outlook.com>
2022-05-20 22:23:05 +0300

    Updated the learning space view to return a single HTML for both authenticated and authenticated users and updated the context information to include more information related to the learning space

--- swe573/coLearn/views.py
+++ swe573/coLearn/views.py
@@ -3,19 +3,37 @@ from django.shortcuts import render
 from django.template import loader
 from .models import LearningSpace
 
+# Learning Space views.
+
 def learning_space_view(request, learning_space_id):
 
   learningSpace = LearningSpace.objects.get(pk=learning_space_id)
-  topics = learningSpace.topics.all()
+
+  # Obtain quizzes and questions.
+  quizzes = learningSpace.quizzes.all()
+  questions = learningSpace.questions.all()
+
+  # Obtain the subscribers using the LearningSpace model.
+  subscribers = learningSpace.subscribers.all()
+
+  # TODO: Dummy value until authenticated is implemented.
+  user_authenticated = False
+
+  relatedSpaces = LearningSpace.objects.filter(keywords=learningSpace.keywords)
 
   context = {
     'title' : learningSpace.title,
     'overview' : learningSpace.overview,
     'prerequisites' : learningSpace.prerequisites,
-    'topics': topics
+    'id': learningSpace.id,
+    'quizzes': quizzes,
+    'questions': questions,
+    'subscribers': subscribers,
+    'related_spaces': relatedSpaces,
+    'user_authenticated': user_authenticated
   }
 
-  return render(request, 'learningSpace/learning_space_unauth.html', context)
+  return render(request, 'learningSpace/learning_space.html', context)
 
 def explore_view(request):
 
