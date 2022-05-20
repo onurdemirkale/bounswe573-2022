fbcdca2031ec63b403b443609eea9edc899b47ad
Rhonur <onur.demirkale@outlook.com>
2022-05-20 22:42:03 +0300

    Updated the CoLearnUser to include a profile picture

--- swe573/coLearn/models.py
+++ swe573/coLearn/models.py
@@ -13,6 +13,7 @@ class CoLearnUser(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE) # Used to extend the Auth User.
   interests = ArrayField(models.CharField(max_length=100), blank=True, null=True)
   background = models.TextField(max_length=1000, blank=True)
+  profile_picture = models.FileField(blank=True) 
   bio = models.TextField(max_length=1000, blank=True)
   birth_date = models.DateField(null=True, blank=True)
 
