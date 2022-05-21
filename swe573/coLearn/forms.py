987d333fccfff4c7eb3704139842b0abe05791ce
Rhonur <onur.demirkale@outlook.com>
2022-05-21 20:48:32 +0300

    Imported SimpleArrayField postgres form and removed ArrayField model. Using ArrayField model led to parsing issues, fixes #45

--- swe573/coLearn/forms.py
+++ swe573/coLearn/forms.py
@@ -1,9 +1,9 @@
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.forms import SimpleArrayField
 from django import forms
 
 class LearningSpaceForm(forms.Form):
   title = forms.CharField(max_length=100)
   overview = forms.CharField(max_length=1000)
-  prerequisites = ArrayField(forms.CharField(max_length=100), blank=True)
-  keywords = ArrayField(forms.CharField(max_length=100), blank=True)
+  prerequisites = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
+  keywords = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
   thumbnail = forms.FileField()
