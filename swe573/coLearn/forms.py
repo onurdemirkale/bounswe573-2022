b1b418d406a030324bd001be4b82c4a5511413ee
Rhonur <onur.demirkale@outlook.com>
2022-05-22 12:04:27 +0300

    Implemented the form used to edit learning spaces

--- swe573/coLearn/forms.py
+++ swe573/coLearn/forms.py
@@ -7,3 +7,9 @@ class LearningSpaceCreateForm(forms.Form):
   prerequisites = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
   keywords = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
   thumbnail = forms.FileField()
+
+class LearningSpaceEditForm(forms.Form):
+  title = forms.CharField(max_length=100)
+  overview = forms.CharField(max_length=1000)
+  prerequisites = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
+  keywords = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
