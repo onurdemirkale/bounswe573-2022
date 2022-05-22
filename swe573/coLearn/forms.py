cd58a860b2a5389af290f6a41ed7f75e848dfec9
Rhonur <onur.demirkale@outlook.com>
2022-05-22 12:12:02 +0300

    Implemented the form used to sign in users

--- swe573/coLearn/forms.py
+++ swe573/coLearn/forms.py
@@ -13,3 +13,7 @@ class LearningSpaceEditForm(forms.Form):
   overview = forms.CharField(max_length=1000)
   prerequisites = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
   keywords = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
+
+class SignInForm(forms.Form):
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}))
