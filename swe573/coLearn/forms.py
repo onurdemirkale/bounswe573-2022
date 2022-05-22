0782e9ec4f7c505c5b9daa639bb01852ad4f1384
Rhonur <onur.demirkale@outlook.com>
2022-05-22 13:39:02 +0300

    Imported get_user_model from auth to access User model, added a self function to validate username for SignInForm

--- swe573/coLearn/forms.py
+++ swe573/coLearn/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.postgres.forms import SimpleArrayField
+from django.contrib.auth import get_user_model
 from django import forms
 
 class LearningSpaceCreateForm(forms.Form):
@@ -14,9 +15,18 @@ class LearningSpaceEditForm(forms.Form):
   prerequisites = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
   keywords = SimpleArrayField(forms.CharField(max_length=100), delimiter=',')
 
+User = get_user_model() # User model can't be imported. It has to be called through function instead.
+
 class SignInForm(forms.Form):
-    username = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
-    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}))
+  username = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
+  password = forms.CharField(label='', widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}))
+
+  def validate_username(self):
+    username = self.clenaned_data.get("username")
+    username_query = User.objects.filter(username_iexact=username)
+    if not username_query.exists():
+      raise forms.ValidationError('This username already exists.')
+    return username
 
 class SignUpForm(forms.Form):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={"class": "form-control"}))
