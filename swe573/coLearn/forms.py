ac501a1d000f6cba7dfce53be50d787c6b4a48db
Rhonur <onur.demirkale@outlook.com>
2022-05-22 12:12:15 +0300

    Implemented the form used to sign up users

--- swe573/coLearn/forms.py
+++ swe573/coLearn/forms.py
@@ -17,3 +17,11 @@ class LearningSpaceEditForm(forms.Form):
 class SignInForm(forms.Form):
     username = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
     password = forms.CharField(label='', widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}))
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={"class": "form-control"}))
