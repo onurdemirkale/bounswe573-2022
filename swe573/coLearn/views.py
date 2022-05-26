fdf1f05c8581a720e3aa892d7555c1f7739a8a90
Rhonur <onur.demirkale@outlook.com>
2022-05-26 17:21:16 +0300

    Implemented user authentication information to learning_space_create_view to correctly contain profile href in the navbar, fixes #57

--- swe573/coLearn/views.py
+++ swe573/coLearn/views.py
@@ -54,7 +54,13 @@ def learning_space_create_view(request):
       if(space_created):
         return redirect('/learningspace/%d' % space_created.id )
   else:
-    context = {}
+    user_id = None
+  
+    if request.user.is_authenticated:
+      user_authenticated = True
+      user_id = request.user.id
+
+    context = {'user_authenticated': user_authenticated, 'user_id': user_id}
     return render(request, 'learningSpace/learning_space_create.html', context)
 
 @login_required
