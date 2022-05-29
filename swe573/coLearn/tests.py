1e8e4ba12573e401b347b9136e7f1d8785c19e13
Rhonur <onur.demirkale@outlook.com>
2022-05-30 02:18:24 +0300

    Implemented unit test for querying overviews through search view

--- swe573/coLearn/tests.py
+++ swe573/coLearn/tests.py
@@ -328,3 +328,16 @@ class Search(TestCase):
     self.assertEqual(status_code, 200)
     self.assertEqual(redirect_path, subscribe_learning_space_url)
 
+  # Ensure that search view works correctly by querying an overview.
+  def test_query_overview(self):
+    self.client.force_login(self.user_t)
+    subscribe_learning_space_url = '/search/'
+    search_query = 'enjoy'
+    response = self.client.get(subscribe_learning_space_url, {'query':search_query }, follow=True)
+    status_code = response.status_code
+    redirect_path = response.request.get('PATH_INFO')
+    learning_space=response.context['learning_spaces']
+    learning_space_count=learning_space.count()
+    self.assertEqual(learning_space_count, 1)
+    self.assertEqual(status_code, 200)
+    self.assertEqual(redirect_path, subscribe_learning_space_url)
